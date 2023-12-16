c357bebf83767b2016e1e81e03a2fb70a9512c21
Ray Deck <ray@raydeck.com>
2023-12-16 14:25:54 -0500

    feat: implement commander

--- bin.mjs
+++ bin.mjs
@@ -3,21 +3,27 @@ import { Command } from "commander";
 import { join } from "node:path";
 import { readFileSync } from "node:fs";
 
-import packageInfo from "./package.json" assert { type: "json" };
-const { version } = packageInfo;
+const { version } = JSON.parse(readFileSync("./package.json", "utf8"));
 const program = new Command();
 program.version(version);
 program.name("Ollama CLI Interface");
 program.usage("<prompt> [options]");
-
-program.addOption("-m, --model <model>", "model to use", "llama2");
-
+program.option("-m, --model <model>", "model to use", "llama2");
+program.option("-j --json", "output as json");
 program.parse(process.argv);
-prompt = program.args[0];
-model = program.model;
-(async () => {
-  const ollama = new Ollama();
-  await ollama.setModel(model);
-  const { output } = await ollama.generate(prompt);
-  console.log(output);
-})();
+const prompt = program.args[0];
+const model = program.getOptionValue("model");
+const isJson = program.getOptionValue("json");
+if (!prompt) program.help();
+else
+  (async () => {
+    const ollama = new Ollama();
+    await ollama.setModel(model);
+    const obj = await ollama.generate(prompt);
+    if (isJson) {
+      console.log(JSON.stringify(obj));
+      return;
+    } else {
+      console.log(obj.output);
+    }
+  })();

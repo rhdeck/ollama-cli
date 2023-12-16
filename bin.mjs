c1ecb9e071d671aebaaac7ded8468c8933e8523d
Ray Deck <ray@raydeck.com>
2023-12-16 14:39:44 -0500

    docs: add comments

--- bin.mjs
+++ bin.mjs
@@ -4,19 +4,23 @@ import { Command } from "commander";
 import { join, dirname } from "node:path";
 import { readFileSync } from "node:fs";
 import { fileURLToPath } from "node:url";
-
+//#region get version from package.json
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const { version } = JSON.parse(
   readFileSync(join(__dirname, "package.json"), "utf8")
 );
+//#endregion
+//#region command line setup
 const program = new Command();
 program.version(version);
 program.name("Ollama CLI Interface");
 program.usage("<prompt> [options]");
 program.option("-m, --model <model>", "model to use", "llama2");
 program.option("-j --json", "output as json");
+//#endregion
+//#region command line execution
 program.parse(process.argv);
 const prompt = program.args[0];
 const model = program.getOptionValue("model");
@@ -34,3 +38,4 @@ else
       console.log(obj.output);
     }
   })();
+//#endregion
